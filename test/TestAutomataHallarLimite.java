39cf547af3d152d63b954a65d28885bab7a9a888
Jhon Quirama <quirama80@gmail.com>
2019-09-02 15:23:36 -0500

    Pruebas unitarias corregidas

---
+++ test/TestAutomataHallarLimite.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import Controlador.Automata_hallarLimite;
+import Modelo.Flujo_caracteres;
+import Modelo.Lexema;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class TestAutomataHallarLimite {
+    
+     @Test
+    public void TestAutomataHallarLimiteCorrecto() {
+       Automata_hallarLimite fun = new Automata_hallarLimite();
+        char[] caracteres = "hallarLimite".toCharArray();
+        Lexema resultado = fun.inicio(new Flujo_caracteres(0, caracteres));
+        int esperado = 1;
+        int resultadoMetodo;
+        if (resultado != null) {
+            resultadoMetodo = 1;
+        } else {
+            resultadoMetodo = 0;
+        }
+
+        assertEquals(esperado, resultadoMetodo);
+
+    }
+
+    @Test
+    public void TestAutomataHallarLimiteInCorrecto() {
+       Automata_hallarLimite fun = new Automata_hallarLimite();
+        char[] caracteres = "hallarLimitede".toCharArray();
+        Lexema resultado = fun.inicio(new Flujo_caracteres(0, caracteres));
+        int esperado = 0;
+        int resultadoMetodo;
+        if (resultado != null) {
+            resultadoMetodo = 1;
+        } else {
+            resultadoMetodo = 0;
+        }
+
+        assertEquals(esperado, resultadoMetodo);
+
+    }
+    
+}
